5dc4ac3c67a6a6d70e3632ff778624d0ff7b9326
bryan lesht <bryan.lesht@colorado.edu>
2020-01-21 17:59:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 first_number = 5
 second_number = 2 
-def sum(first_number,second_number)
-  puts first_number+second_number
+def sumfunc(first,second)
+  puts first+second
 end
-sum(5,7)
+sum = sumfunc(first_number,second_number)
