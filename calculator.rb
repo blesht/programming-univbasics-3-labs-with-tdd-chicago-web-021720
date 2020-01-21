535fc1de7e07407176ffc597aa7c972d9a84cd2c
bryan lesht <bryan.lesht@colorado.edu>
2020-01-21 18:02:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,7 @@ def sumfunc(first,second)
   first+second
 end
 sum = sumfunc(first_number,second_number)
+def difFunc(first,second)
+  first-second
+end
+difference = difFunc(first_number,second_number)
